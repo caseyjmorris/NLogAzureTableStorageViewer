913079c8d1d1782f4c02514a20de2b932a44095d
Casey J. Morris <caseyjmorris@gmail.com>
2016-02-13 14:59:00 -0500

    Basic details screen

---
+++ Azure.NLog.Query.UI/Controllers/DetailsController.cs
@@ -0,0 +1,21 @@
+﻿using Azure.NLog.Query.UI.ViewModels;
+using NLog.Extensions.AzureTableStorage;
+
+namespace Azure.NLog.Query.UI.Controllers
+{
+  public class DetailsController : BaseViewModel
+  {
+    private LogEntity log;
+
+    public LogEntity Log
+    {
+      get { return this.log; }
+      set
+      {
+        if (Equals(value, this.log)) return;
+        this.log = value;
+        this.OnPropertyChanged(nameof(this.Log));
+      }
+    }
+  }
+}
